f3d06e63e1e614e27e87a691fedba4341c0a684f
Gabriel mena <gabriel.mena.jofre@gmail.com>
2026-01-19 17:41:18 -0300

    asdasd

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
           {phase === "closed" && (
             <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
               <Image
-                src="ga.png"
+                src="/ga.png"
                 alt="Sello G&A"
                 width={250}
                 height={250}
